8128668daa2fa71a1b676c358e362b099385d4de
Alexander Byankin <samel@bk.ru>
2021-08-21 08:37:38 +1100

    Initial project setup

---
+++ desktop/src/com/github/samelvhatargh/druid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.samelvhatargh.druid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.samelvhatargh.druid.DruidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new DruidGame(), config);
+	}
+}
