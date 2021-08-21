03bc23883863278e520d05b7dd2ca12202a0c575
Alexander Byankin <samel@bk.ru>
2021-08-22 09:33:04 +1100

    Trying to create jar

---
+++ desktop/src/com/github/samelvhatargh/druid/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.github.samelvhatargh.druid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.samelvhatargh.druid.Config;
+import com.github.samelvhatargh.druid.DruidGame;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Druid and His Circle of Animals");
+        config.setWindowedMode(Config.windowWidth, Config.windowHeight);
+        new Lwjgl3Application(new DruidGame(), config);
+    }
+}
