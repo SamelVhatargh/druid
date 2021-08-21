bdd9062b039f706b9449ffea40cef03034f78869
Alexander Byankin <samel@bk.ru>
2021-08-21 12:33:25 +1100

    Added camera

--- core/src/com/github/samelvhatargh/druid/DruidGame.kt
+++ core/src/com/github/samelvhatargh/druid/DruidGame.kt
@@ -1,5 +1,6 @@
 package com.github.samelvhatargh.druid
 
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.github.samelvhatargh.druid.screens.PlayScreen
@@ -9,9 +10,10 @@ import ktx.app.KtxScreen
 class DruidGame : KtxGame<KtxScreen>() {
     private val batch by lazy { SpriteBatch() }
     private val img by lazy { Texture("badlogic.jpg") }
+    private val camera by lazy { OrthographicCamera(16f, 9f) }
 
     override fun create() {
-        addScreen(PlayScreen(batch, img))
+        addScreen(PlayScreen(batch, img, camera))
         setScreen<PlayScreen>()
     }
 
--- core/src/com/github/samelvhatargh/druid/screens/PlayScreen.kt
+++ core/src/com/github/samelvhatargh/druid/screens/PlayScreen.kt
@@ -1,15 +1,16 @@
 package com.github.samelvhatargh.druid.screens
 
+import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxScreen
 import ktx.graphics.use
 
-class PlayScreen(private val batch: SpriteBatch, private val img: Texture) : KtxScreen {
+class PlayScreen(private val batch: SpriteBatch, private val img: Texture, private val camera: Camera) : KtxScreen {
 
     override fun render(delta: Float) {
-        batch.use {
-            batch.draw(img, 0f, 0f)
+        batch.use(camera.combined) {
+            batch.draw(img, -.5f, -.5f, 1f, 1f)
         }
     }
 }
