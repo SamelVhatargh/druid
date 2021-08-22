d3fec3a2da1281413a0023acf96e7f68f0e3a6df
Alexander Byankin <samel@bk.ru>
2021-08-22 15:24:38 +1100

    Added representation of all collected animals to win screen

--- core/src/com/github/samelvhatargh/druid/screens/WinScreen.kt
+++ core/src/com/github/samelvhatargh/druid/screens/WinScreen.kt
@@ -9,8 +9,21 @@ import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.github.samelvhatargh.druid.Config
 import com.github.samelvhatargh.druid.DruidGame
+import com.github.samelvhatargh.druid.components.Animal
+import com.github.samelvhatargh.druid.components.CollectedAnimal
+import com.github.samelvhatargh.druid.components.Position
+import com.github.samelvhatargh.druid.getCollectedAnimals
+import com.github.samelvhatargh.druid.systems.AnimalRender
+import com.github.samelvhatargh.druid.systems.LevelRender
+import com.github.samelvhatargh.druid.systems.Sound
+import com.github.samelvhatargh.druid.systems.SoundEffects
 import ktx.actors.onClick
+import ktx.ashley.allOf
+import ktx.ashley.exclude
+import ktx.ashley.get
+import ktx.ashley.getSystem
 import ktx.scene2d.*
+import kotlin.math.floor
 
 class WinScreen(
     engine: Engine,
@@ -23,7 +36,16 @@ class WinScreen(
         Stage(FitViewport(Config.windowWidth.toFloat(), Config.windowHeight.toFloat()))
     }
 
+    var animalPopSpeed = .05f
+    var timePast = 0f
+    var animalsRendered = 0
+
     override fun show() {
+        engine.getSystem<AnimalRender>().setProcessing(true)
+        engine.getSystem<LevelRender>().setProcessing(true)
+
+        engine.removeAllEntities(allOf(Animal::class).exclude(CollectedAnimal::class).get())
+
         val tableWidth = 1000f
         val padding = 50f
         inputMultiplexer.addProcessor(stage)
@@ -85,7 +107,29 @@ class WinScreen(
 
     override fun render(delta: Float) {
         super.render(delta)
+        renderAnimals(delta)
         stage.act()
         stage.draw()
+    }
+
+    private fun renderAnimals(delta: Float) {
+        val rowSize = 17f
+        timePast += delta
+        if (timePast >= animalPopSpeed) {
+            val animals = engine.getCollectedAnimals().sortedBy {
+                -it[Animal.mapper]!!.level
+            }
+            if (animals.isNotEmpty() && animalsRendered < animals.size) {
+                val animal = animals.get(animalsRendered)
+                val dx = animalsRendered % rowSize
+                animal[Position.mapper]!!.vec.set(-12f + dx * 1.5f, -5.5f - (floor(animalsRendered/ rowSize) * 1.5f))
+                engine.getSystem<SoundEffects>().play(Sound.POP)
+
+
+                animalsRendered++
+            }
+
+            timePast = 0f
+        }
     }
 }
