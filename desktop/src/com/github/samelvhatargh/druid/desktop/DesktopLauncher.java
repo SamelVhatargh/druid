081261739f8a3af7359242fdb119d04109a9e5a0
Alexander Byankin <samel@bk.ru>
2021-08-22 18:05:43 +1100

    Added icons

--- desktop/src/com/github/samelvhatargh/druid/desktop/DesktopLauncher.java
+++ desktop/src/com/github/samelvhatargh/druid/desktop/DesktopLauncher.java
@@ -6,10 +6,11 @@ import com.github.samelvhatargh.druid.Config;
 import com.github.samelvhatargh.druid.DruidGame;
 
 public class DesktopLauncher {
-    public static void main (String[] arg) {
+    public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Druid and His Circle of Animals");
         config.setWindowedMode(Config.windowWidth, Config.windowHeight);
+        config.setWindowIcon("icons/icon16.png", "icons/icon32.png", "icons/icon48.png", "icons/icon64.png", "icons/icon128.png");
         new Lwjgl3Application(new DruidGame(), config);
     }
 }
